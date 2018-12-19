d218435c685a1047cbcba3a79a63de11e77a876d
Rens Oliemans <rens.oliemans@gmail.com>
2018-12-19 22:55:37 +0100

    :art: Move generate_animation to static method

--- webserver_handler.py
+++ webserver_handler.py
@@ -7,24 +7,6 @@ from config import FPS
 from color import Color
 
 
-def generate_animation(start_color, final_color, duration, ease):
-    """
-    Generates an animation for the controller.
-
-    :param start_color: color before the animation
-    :param final_color: supposed color after the animation
-    :param duration: duration of the animation
-    :param ease: the easing function to use
-    :return: list of colors in the animation
-    """
-    diff = final_color - start_color
-    tween = getattr(pytweening, ease)
-    # Get list of colors in the animation
-    animation = [start_color + diff * tween(step)
-                 for step in linspace(0, 1, int(FPS * duration / 1000))]
-    return animation
-
-
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
 
@@ -33,6 +15,7 @@ class ControllerHandler:
     """
     Handler function; used by the webserver, contains bits of controller functionality
     """
+
     def __init__(self, controller, current_color=Color(0, 0, 0)):
         self.controller = controller
         self.current_color = current_color
@@ -46,8 +29,8 @@ class ControllerHandler:
         color = request_json['color']
         r, g, b = color['r'], color['g'], color['b']
 
-        animation = generate_animation(self.current_color, Color(r, g, b),
-                                       request_json['duration'], request_json['ease'])
+        animation = self.generate_animation(self.current_color, Color(r, g, b),
+                                            request_json['duration'], request_json['ease'])
         self.play_animation(animation)
 
         # Last color of the animation is the 'final' color, so the current color of the controller
@@ -63,3 +46,21 @@ class ControllerHandler:
         self.controller.send_start(0, [r, g, b, 0, 0, 0])
         self.controller.make_frame()
         self.controller.make_frame()
+
+    @staticmethod
+    def generate_animation(start_color, final_color, duration, ease):
+        """
+        Generates an animation for the controller.
+
+        :param start_color: color before the animation
+        :param final_color: supposed color after the animation
+        :param duration: duration of the animation
+        :param ease: the easing function to use
+        :return: list of colors in the animation
+        """
+        diff = final_color - start_color
+        tween = getattr(pytweening, ease)
+        # Get list of colors in the animation
+        animation = [start_color + diff * tween(step)
+                     for step in linspace(0, 1, int(FPS * duration / 1000))]
+        return animation
