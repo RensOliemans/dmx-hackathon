8f4d26ec8228040ee373880c9f078978fdcc2d89
Rens Oliemans <rens.oliemans@gmail.com>
2019-01-23 21:43:36 +0100

    :art: More readable indentation

--- log.py
+++ log.py
@@ -3,11 +3,13 @@ from loguru import logger
 from config import LOGLVL
 
 logger.remove()
-logger.add(sys.stdout, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> "
-                                             "| <level>{level:<8}</level> "
-                                             "| <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
-                                             "<level>{message}</level>", level=LOGLVL)
-logger.add("logs/log{time}.log", colorize=True, backtrace=False, format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> "
-                                                                        "| <level>{level:<8}</level> "
-                                                                        "| <cyan>{name}</cyan>:<cyan>{line}</cyan> - "
-                                                                        "<level>{message}</level>", level="DEBUG")
+logger.add(sys.stdout, colorize=True, level=LOGLVL,
+           format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> "
+                  "| <level>{level:<8}</level> "
+                  "| <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+                  "<level>{message}</level>")
+logger.add("logs/log{time}.log", colorize=True, backtrace=False, level="DEBUG", retention="10 days",
+           format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> "
+                  "| <level>{level:<8}</level> "
+                  "| <cyan>{name}</cyan>:<cyan>{line}</cyan> - "
+                  "<level>{message}</level>")
